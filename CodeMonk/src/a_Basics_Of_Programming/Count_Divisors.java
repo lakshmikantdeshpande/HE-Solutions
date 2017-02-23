6e7cd8564ca522b18603c473c420902e662ca2e1
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-02-23 17:17:58 +0530

    Basics of Input-Output
    CodeMonk started

---
+++ CodeMonk/src/a_Basics_Of_Programming/Count_Divisors.java
@@ -0,0 +1,31 @@
+package a_Basics_Of_Programming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Count_Divisors {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str = new StringTokenizer(br.readLine());
+
+		int i = Integer.parseInt(str.nextToken());
+		int j = Integer.parseInt(str.nextToken());
+		int m = Integer.parseInt(str.nextToken());
+
+		if (m == 1)
+			System.out.println(j - i + 1);
+		else {
+			int counter = 0;
+			for (int p = i; p <= j; p++) {
+				if (p % m == 0)
+					counter++;
+			}
+			System.out.println(counter);
+		}
+
+		str = null;
+		br = null;
+		System.gc();
+	}
+}
---
+++ CodeMonk/src/a_Basics_Of_Programming/Roy_and_Profile_Picture.java
@@ -0,0 +1,37 @@
+package a_Basics_Of_Programming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Roy_and_Profile_Picture {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str = null;
+		StringBuilder strb = new StringBuilder();
+
+		final int L = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+
+		while (N-- > 0) {
+			str = new StringTokenizer(br.readLine());
+			int W = Integer.parseInt(str.nextToken());
+			int H = Integer.parseInt(str.nextToken());
+
+			if (W < L || H < L)
+				strb.append("UPLOAD ANOTHER" + "\n");
+			else {
+				if (W == H)
+					strb.append("ACCEPTED" + "\n");
+				else
+					strb.append("CROP IT" + "\n");
+			}
+		}
+		System.out.print(strb.toString());
+
+		strb = null;
+		str = null;
+		br = null;
+		System.gc();
+	}
+}
