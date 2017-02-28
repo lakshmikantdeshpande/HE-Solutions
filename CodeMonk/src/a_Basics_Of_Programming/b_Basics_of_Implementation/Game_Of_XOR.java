3181eee60688ac21cb4e98a17f36ad0098555bb5
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-02-28 15:31:53 +0530

    XOR code incomplete

---
+++ CodeMonk/src/a_Basics_Of_Programming/b_Basics_of_Implementation/Game_Of_XOR.java
@@ -0,0 +1,25 @@
+package a_Basics_Of_Programming.b_Basics_of_Implementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Game_Of_XOR {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		while (N-- > 0) {
+			int size = Integer.parseInt(br.readLine());
+			for (int i = 0; i < size; i++) {
+				for (int j = i; j < size; j++) {
+					
+					for (int k = i; k <= j; k++) 
+				}
+			}
+		}
+
+		br = null;
+		System.gc();
+	}
+}
